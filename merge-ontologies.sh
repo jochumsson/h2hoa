136818e2a97afea4a5716495a737fd353688e2a9
Thorvaldur Jochumsson <thorvaldur.jochumsson@mdu.se>
2026-01-22 13:31:42 +0100

    Update with h2ho-a with sections and clean comments

--- merge-ontologies.sh
+++ merge-ontologies.sh
@@ -35,7 +35,7 @@ echo "" >> "$OUT"
 # 4. Append restrictions WITHOUT HEADER
 ############################################################
 # Same logic: remove everything before the first real triple
-sed -n '/^h2ho:/,$p' "$H2HOA" >> "$OUT"
+sed -n '/^# H2HO-A - start of the ontolgy/,$p' "$H2HOA" >> "$OUT"
 
 echo ""
 echo "Done. Output written to $OUT"
